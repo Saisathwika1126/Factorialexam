674ac2ed92a5b58779a83bb1dc9316149dc40c64
Saisathwika1126 <saisathwikadurishetty1126@gmail.com>
2026-09-07 09:53:37 +0530

    first commit

---
+++ TestFactorial.java
@@ -0,0 +1,18 @@
+public class Factorial {
+    public static void main(String[] args) {
+        try{
+            if(Factorial.compute(5)!=120){
+                System.out.println("Test failed: Factorial of 5 should be 120");
+
+            } 
+            if(Factorial.compute(0)!=1){
+                
+                else {
+                System.out.println("Test cases passed");
+            }
+            catch(Exception e){
+                System.out.println("Test failed: "+e.getMessage());
+        }
+
+    }
+}
