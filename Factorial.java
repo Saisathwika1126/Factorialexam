1dcfbe387848a1451c40abd82467b3c35c0d37af
Saisathwika1126 <saisathwikadurishetty1126@gmail.com>
2026-09-07 10:16:19 +0530

    Refactor Factorial class for improved readability

--- Factorial.java
+++ Factorial.java
@@ -1,17 +1,20 @@
-public class Factorial{
-    public static long compute(int n){
-        if(n<0){
+public class Factorial {
+    public static long compute(int n) {
+        if (n < 0) {
             throw new IllegalArgumentException("Negative numbers are not allowed");
         }
-        long result=1;
-        for(i=1;i<=n;i++){
-            result=result*i;
-        }
-        return result;
 
+        long result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+
+        return result;
     }
-    public static void main(String[] args){
-        int n=5;
-        System.out.println("Factorial of "+n+" is "+compute(n));
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("Factorial of " + n + " is " + compute(n));
     }
 }
